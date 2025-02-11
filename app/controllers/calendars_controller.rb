72fe19fcddb6f74385031f49f9334d7b403bb8a6
i-shintaro <shintaroiwashita7857@gmail.com>
2025-02-11 13:34:11 +0900

    Revert "Merge pull request #11 from i-shintaro/test2"
    This reverts commit e14605a6fc1535eae3b5c4fbde567493b270f07d, reversing
    changes made to a6fa570673be7e27f452b51710e3110a32a09d6e.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,6 @@ class CalendarsController < ApplicationController
       today_plans = [] #予定を格納する配列
       plans.each do |plan| 
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-        test
       end
 
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
